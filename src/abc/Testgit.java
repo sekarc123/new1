5da029fe0b05c822107b9d6880b5c5bb004e5b42
sekarc12 <sekarc123@gmail.com>
2017-04-13 16:26:01 -0400

    added

--- src/abc/Testgit.java
+++ src/abc/Testgit.java
@@ -5,8 +5,8 @@ public class Testgit
 	public static void main(String[] args) 
 	{
 		System.out.println("my first git");
-		System.out.println("my first commit  change");
-		
+		System.out.println("my first commit change");
+		System.out.println("hi");
 	}
 
 }
